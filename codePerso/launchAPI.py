be479d6ce3c412051ffa09624027a633e981763a
rodolphe <rodolphe.calvet@telecom-paris.fr>
2022-12-18 16:45:38 +0100

    Chgt pipe

--- codePerso/launchAPI.py
+++ codePerso/launchAPI.py
@@ -2,6 +2,10 @@ import os
 
 from haystack.document_stores import ElasticsearchDocumentStore
 from haystack.utils import clean_wiki_text, convert_files_to_docs, fetch_archive_from_http
+import logging
+
+logging.basicConfig(format="%(levelname)s - %(name)s -  %(message)s", level=logging.DEBUG)#WARNING)
+logging.getLogger("haystack").setLevel(logging.DEBUG)#
 
 host = os.environ.get("ELASTICSEARCH_HOST", "localhost")
 print(f'host : {host}')
@@ -9,8 +13,8 @@ print(f'host : {host}')
 document_store = ElasticsearchDocumentStore(username="", password="", index="document")
 doc_dir = "C:/dataHaystack/shakespeare"
 mod_dir = "C:/dataHaystack/mods_shakespeare"
-wikiWill = "https://github.com/RodolpheCalvet/haystack-code/raw/main/data/wiki.txt"
-fetch_archive_from_http(url=wikiWill, output_dir=doc_dir)
+wikiWill = "https://github.com/RodolpheCalvet/haystack/raw/main/data/wiki.zip"#WilliWiki.pdf"#wiki.txt"
+#fetch_archive_from_http(url=wikiWill, output_dir=doc_dir)
 
 docs = convert_files_to_docs(dir_path=doc_dir, clean_func=clean_wiki_text, split_paragraphs=True)
 
